75da64c1e91125cdac2f952c7b97957484168b4f
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-08-04 01:06:29 +0530

    first commit

---
+++ pokemon-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { Pokemon } from "./components/Pokemon"
+import "./index.css"
+
+export const App = () =>{
+  return (
+    <div className="outer-cont">
+    <Pokemon/>
+    </div>
+
+  )
+
+}
---
+++ pokemon-app/src/components/Pokemon.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react"
+import "../index.css"
+import { PokemonCards } from "./PokemonCards"
+
+export const Pokemon = () => {
+    const [pokemon, setPokemon] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const [search, setSearch] = useState("")
+
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=200";
+
+    const fetchPokemon = async() => {
+        try {
+            const res = await fetch(API)
+            const data = await res.json()
+            // console.log(data);
+
+            const detailedPokemonData = data.results.map(async (currPokemon) => {
+                const res = await fetch(currPokemon.url)
+                const data = await res.json()
+                // console.log(data);
+                return data;
+            })
+            // console.log(detailedPokemonData);
+
+            const detailedResponses = await Promise.all(detailedPokemonData)
+            // console.log(detailedResponses);
+            
+            setPokemon(detailedResponses)
+            setLoading(false)
+            
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+            setError(error)
+        }
+    }
+
+    useEffect(()=>{
+        fetchPokemon()
+    },[])
+
+
+    //Search Function
+    const searchData = pokemon.filter(currPokemon => currPokemon.name.toLowerCase().includes(search.toLowerCase()))
+    
+    if(loading){
+        return(
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+    
+    if(error){
+        return(
+            <div>
+                <h1>Check your URL :{error.message}</h1>
+            </div>
+        )
+    }
+    
+
+    return (
+        <>
+            <section className="container">
+                <header>
+                    <h1>Let's Catch Pokemon</h1>
+                    <h2>made by 😎 Aniket</h2>
+                </header>
+
+                <div className="pokemon-search">
+                    <input type="text" placeholder="Search Pokemon" value={search} onChange={(e)=> setSearch(e.target.value)} />
+                </div>
+
+                <div>
+                    <ul className="cards">
+                        {
+                            //pokemon.map....
+                            searchData.map((currPokemon)=>(
+                                <PokemonCards key={currPokemon.id} pokemonData ={ currPokemon } />
+                            ))
+                        }
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
+
+
+// "https://pokeapi.co/api/v2/"
